abe85f9feac239241c60501489330c820b2862b2
KyungIn Lee <jess.lee2906@gmail.com>
2022-05-08 17:24:00 +1000

    setup Express app to handle data parsing

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-// Packages required for application
+// Import packages required for application
 const fs = require("fs");
 const path = require('path');
 const express = require("express");
-
-//start new server to listen to port value 3000
 const app = express();
-const port = process.env.PORT || 3000;
+//start new server to listen to port value 3000
+const port = process.env.PORT || 3001;
 
-// Setup data parsing
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-
-// Parse incoming data
 app.use(express.json());
 app.use(express.static(__dirname));
+
 require('./routes/routes')(app);
 
-// Listening on port 3000
+// Launch app on port 3001
 app.listen(PORT, () => {
-    console.log(`Listening on PORT: "${PORT}!`);
-}); 
+  console.log(`App listening at http://localhost:${PORT}`);
+});
